44e289168226cd393a56c90681e856bdffcabe1f
Harshal Sahgel <129950309+HarshalSahgel@users.noreply.github.com>
2024-02-28 22:36:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const url = window.location.href;
+
+let letterCount = 0;
+
+for(let i = 0; i < url.length; i++){
+	if((url[i] >= "a" && url[i] <= "z") || (url[i] >= "A" && url[i] <= "Z")){
+		letterCount++;
+	} 
+}
+
+console.log(letterCount);
